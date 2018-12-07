88927c2ec03e1129c3f94db586ee7d20ffb92a90
stephanstruebing <stephan.struebing@gmx.at>
2018-12-07 18:31:47 +0100

    Add: loops

---
+++ uebung08/loops.py
@@ -0,0 +1,24 @@
+# Autor: Stephan Struebing
+
+"""
+    Python Version 2.7.10
+    Loops Beispiele
+"""
+"""
+value_one = 3
+value_two = 4
+
+if value_one != value_two or value_one == value_two:
+    print ("Yuhu")
+else:
+    print ("Falsch")
+"""
+
+for number in range (0, 5):
+    if number == 2:
+        print (number)
+        break
+    else:
+        print (number)
+
+    print ("Restcode in der Schleife")
