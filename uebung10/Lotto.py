54863e24a1598cdfc5e172d1362427e01f74cc67
stephanstruebing <stephan.struebing@gmx.at>
2019-01-18 17:52:17 +0100

    Add:Web Lotto

--- uebung10/Lotto.py
+++ uebung10/Lotto.py
@@ -3,12 +3,10 @@ import random
 print ("Welcome to the Lottery generator.")
 
 while True:
-    random_numbers_list = []
 
     number_of_num = int(raw_input("Please enter how many random numbers between 1 and 50 would you like to have: "))
 
     random_numbers = random.sample(range(1, 51), k=number_of_num)
-    random_numbers_list.append(random_numbers)
     print random_numbers
 
     again = raw_input("Would you like to try again? (yes/no)")
@@ -20,5 +18,5 @@ while True:
     else:
         break
 
-print ("Here are your generated numbers: " + str(random_numbers_list))
+print ("Here are your generated numbers: " + str(random_numbers))
 print ("END")
